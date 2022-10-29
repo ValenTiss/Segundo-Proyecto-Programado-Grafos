5170f3716697e121a5546b53f53318dee76c6551
ValenTiss <100385088+ValenTiss@users.noreply.github.com>
2022-10-29 13:40:33 -0400

    Se sube main.cpp

---
+++ main.cpp/main.cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  main.cpp
+//
+//  Created by Valen Tissera on 29/10/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
